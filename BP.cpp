cfd90e732e31bc3fff28e87c1d7c6d530d08c301
ibrahimfe <94334385+ibrahimfe@users.noreply.github.com>
2022-11-13 21:31:27 +0700

    Add files via upload

---
+++ BP.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    char x[2][4] = {
+        {'A', 'B', 'C', 'D'},
+        {'E', 'F', 'G', 'H'}
+    };
+
+    for (int i = 0; i < 2; i++) {
+        cout << "|";
+        for (int j = 0; j<4; j++) {
+            cout<<x[i][j];
+            if (j != 3) {
+                cout << " ";
+            }
+        }
+        cout << "|" << endl;
+    }
+}
