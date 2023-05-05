1c33a24be4a1c2c1b063b809ca58872f5cb2b3e7
ibrahimfe <ibrahiimm.f@gmail.com>
2023-05-05 17:33:11 +0700

    fixing some buggy code

--- django_project/todo_app/models.py
+++ django_project/todo_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 from django.utils import timezone
 from django.urls import reverse
 
@@ -19,13 +18,13 @@ class ToDoItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(defaul=one_week_hence)
+    due_date = models.DateTimeField(default=one_week_hence)
     todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
         return reverse("item-update", args=[str(self.todo_list.id), str(self.id)])
 
-    def __str__(self)
+    def __str__(self):
         return f"{self.title}: due {self.due_date}"
 
     class Meta:
