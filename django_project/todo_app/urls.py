231864637d72b8ded5597005c22839b135858fcf
ibrahimfe <ibrahiimm.f@gmail.com>
2023-05-11 21:28:10 +0700

    add new path for creating new list and new items inn the list

--- django_project/todo_app/urls.py
+++ django_project/todo_app/urls.py
@@ -5,4 +5,17 @@ from todo_app import views
 urlpatterns = [
     path("", views.ListListView.as_view(), name="index"),
     path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
+    # CRUD patterns for ToDoList
+    path("list/add/", views.ListCreate.as_view(), name="list-add"),
+    # CRUD patterns for ToDoItems
+    path(
+        "list/<int:list_id>/item/add/",
+        views.ItemCreate.as_view(),
+        name="item-add",
+    ),
+    path(
+        "list/<int:list_id>/item/<int:pk>/",
+        views.ItemUpdate.as_view(),
+        name="item-update",
+    ),
 ]
