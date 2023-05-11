84ba526ee563b5c862fae29570397267f2ef5886
ibrahimfe <ibrahiimm.f@gmail.com>
2023-05-11 15:38:42 +0700

    adding html and css for to do list and displaying to-do-list items in the web

--- django_project/todo_app/models.py
+++ django_project/todo_app/models.py
@@ -1,9 +1,13 @@
-from django.db import models
+# todo_list/todo_app/models.py
 from django.utils import timezone
+
+from django.db import models
 from django.urls import reverse
+
 
 def one_week_hence():
     return timezone.now() + timezone.timedelta(days=7)
+
 
 class ToDoList(models.Model):
     title = models.CharField(max_length=100, unique=True)
@@ -14,6 +18,7 @@ class ToDoList(models.Model):
     def __str__(self):
         return self.title
 
+
 class ToDoItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
@@ -22,7 +27,9 @@ class ToDoItem(models.Model):
     todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
 
     def get_absolute_url(self):
-        return reverse("item-update", args=[str(self.todo_list.id), str(self.id)])
+        return reverse(
+            "item-update", args=[str(self.todo_list.id), str(self.id)]
+        )
 
     def __str__(self):
         return f"{self.title}: due {self.due_date}"
--- django_project/todo_app/urls.py
+++ django_project/todo_app/urls.py
@@ -1,5 +1,8 @@
+# todo_list/todo_app/urls.py
 from django.urls import path
-from . import views
+from todo_app import views
 
-
-urlpatterns = [path("", views.ListListView.as_view(), name="index")]
+urlpatterns = [
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
+]
